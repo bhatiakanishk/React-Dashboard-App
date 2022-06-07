bab31a8f4eb2245f290e4f6c5bac3c50e76a7ab0
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-07 17:40:48 +0530

    Update App.js

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css'
 
 const App = () => {
   return (
-    <h1 className="underline text-3xl">App</h1>
+    <div>
+        <BrowserRouter>
+            <div className="flex relative dark:bg-main-dark-bg">
+                <div className="fixed right-4 bottom-4" style={{ xIndex: '1000'}}>
+                    <TooltipComponent content="Settings" position="Top">
+                        <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={background}>
+                            <FiSettings>
+
+                            </FiSettings>
+                        </button>
+                    </TooltipComponent>
+                </div>
+            </div>
+        </BrowserRouter>
+    </div>
   )
 }
 
