8ec09aaa084297db6caf3c84fc4c14b3233807ac
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-08 17:54:17 +0530

    Update Navbar.jsx

--- dashboard-app/src/components/Navbar.jsx
+++ dashboard-app/src/components/Navbar.jsx
@@ -27,10 +27,29 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, handleClick } = useStateContext();
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
-      <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon = {<AiOutLineMenu />}/>
+      <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon = {<AiOutlineMenu />}/>
+      
+      <div className="flex">      
+      <NavButton title="Cart" customFunc={() => handleClick('cart')} color="blue" icon ={<FiShoppingCart />}/>
+      
+      <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color="blue" icon ={<BsChatLeft />}/>
+
+      <NavButton title="Notification" dotColor="#03C9D7" customFunc={() => handleClick('notification')} color="blue" icon ={<RiNotification3Line />}/>
+      
+      <TooltipComponent content="Profile" position="BottomCenter">
+        <div className="flex item-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => handleClick('userProfile')}>
+          <img className="rounded-full w-8 h-8" src={avatar} />
+          <p>
+            <span className="text-gray-400 text-14">Hi, </span> {' '}
+            <span className="text-gray-400 font-bold ml-1 text-14">Kanishk</span>
+          </p>
+          <MdKeyboardArrowDown className="text-gray-400 text-14"/>
+        </div>
+      </TooltipComponent>
+      </div>
     </div>
   )
 }
