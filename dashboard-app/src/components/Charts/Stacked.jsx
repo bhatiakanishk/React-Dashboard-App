f038cfda801a73ebf6d5ee8d45a044d4ab4f8b08
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-10 18:22:38 +0530

    Update Stacked.jsx

--- dashboard-app/src/components/Charts/Stacked.jsx
+++ dashboard-app/src/components/Charts/Stacked.jsx
@@ -3,9 +3,23 @@ import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Leg
 
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
 
-const Stacked = () => {
+const Stacked = ({ width, height }) => {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+    width={width}
+    height={height}
+    id="charts"
+    primaryXAxis={stackedPrimaryXAxis}
+    primaryYAxis={stackedPrimaryYAxis}
+    chartArea={{ border: { width: 0 }}}
+    tooltip={{ enabled: true }}
+    legendSettings={{ background: 'white'}}>
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]}/>
+      <SeriesCollectionDirective>
+        { stackedCustomSeries.map((item, index) => 
+        <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
