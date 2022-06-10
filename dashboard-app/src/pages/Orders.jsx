35e74e48adb8d66a059055eaec6975e634a0b5ed
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-10 18:39:48 +0530

    Update Orders.jsx

--- dashboard-app/src/pages/Orders.jsx
+++ dashboard-app/src/pages/Orders.jsx
@@ -7,12 +7,13 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging>
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging allowSorting>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport ]}/>
       </GridComponent>
     </div>
   )
