28e5401602060f28eb342ea84aeadf20f4824808
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-10 19:42:13 +0530

    Update Employees.jsx

--- dashboard-app/src/pages/Employees.jsx
+++ dashboard-app/src/pages/Employees.jsx
@@ -7,7 +7,7 @@ import { Header } from '../components';
 const Employees = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Orders" />
+      <Header category="Page" title="Employees" />
       <GridComponent dataSource={employeesData} allowPaging allowSorting toolbar={[ 'Search' ]} width="auto">
         <ColumnsDirective>
           {employeesGrid.map((item, index) => (
