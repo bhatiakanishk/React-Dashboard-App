fedba9e9df0a768cb53b4105295d84d6a84ee75d
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-10 22:26:42 +0530

    components

--- dashboard-app/src/components/Navbar.jsx
+++ dashboard-app/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
@@ -27,49 +28,63 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked, screenSize, setScreenSize } = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
 
   useEffect(() => {
-    const handleResize = () => setScreenSize(window.innerWidth); window.addEventListener('resize', handleResize); handleResize(); return () => window.removeEventListener('resize, handleResize');
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   useEffect(() => {
-    if(screenSize <= 900){
+    if (screenSize <= 900) {
       setActiveMenu(false);
-    } else{
+    } else {
       setActiveMenu(true);
     }
   }, [screenSize]);
 
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
+
   return (
-    <div className="flex justify-between p-2 md:mx-6 relative">
-      <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon = {<AiOutlineMenu />}/>
-      
-      <div className="flex">      
-      <NavButton title="Cart" customFunc={() => handleClick('cart')} color="blue" icon ={<FiShoppingCart />}/>
-      
-      <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color="blue" icon ={<BsChatLeft />}/>
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
 
-      <NavButton title="Notification" dotColor="#03C9D7" customFunc={() => handleClick('notification')} color="blue" icon ={<RiNotification3Line />}/>
-      
-      <TooltipComponent content="Profile" position="BottomCenter">
-        <div className="flex item-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => handleClick('userProfile')}>
-          <img className="rounded-full w-8 h-8" src={avatar} />
-          <p>
-            <span className="text-gray-400 text-14">Hi, </span> {' '}
-            <span className="text-gray-400 font-bold ml-1 text-14">Kanishk</span>
-          </p>
-          <MdKeyboardArrowDown className="text-gray-400 text-14"/>
-        </div>
-      </TooltipComponent>
+      <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
+      <div className="flex">
+        <NavButton title="Cart" customFunc={() => handleClick('cart')} color={currentColor} icon={<FiShoppingCart />} />
+        <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color={currentColor} icon={<BsChatLeft />} />
+        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')}
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
 
-      {isClicked.cart && <Cart />} 
-      {isClicked.chat && <Chat />} 
-      {isClicked.notification && <Notification />} 
-      {isClicked.userProfile && <UserProfile />} 
+        {isClicked.cart && (<Cart />)}
+        {isClicked.chat && (<Chat />)}
+        {isClicked.notification && (<Notification />)}
+        {isClicked.userProfile && (<UserProfile />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
